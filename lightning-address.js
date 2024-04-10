71fe8103e888ceac6568382ec1e2bab068d508d6
Beat <>
2024-04-10 22:05:01 +0200

    no async function

--- lightning-address.js
+++ lightning-address.js
@@ -1,13 +1,9 @@
 import { validateLightningAddress, LIGHTNING_ADDRESS_INVALID, getUrlOptionsGot, getLightningAddressUri, getGot } from './util.js';
 
-const main = async function () {
-    const lightningAddress = process.argv[2];
-    if (!validateLightningAddress(lightningAddress)) {
-        console.error(LIGHTNING_ADDRESS_INVALID);
-        process.exit();
-    }
-    const options = getUrlOptionsGot(getLightningAddressUri(lightningAddress));
-    getGot(options);
+const lightningAddress = process.argv[2];
+if (!validateLightningAddress(lightningAddress)) {
+    console.error(LIGHTNING_ADDRESS_INVALID);
+    process.exit();
 }
-
-main();
+const options = getUrlOptionsGot(getLightningAddressUri(lightningAddress));
+getGot(options);
